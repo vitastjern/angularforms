e95785a875ed01c63fb8503f335b2234f320ed37
Anna Eklund <vitastjern@gmail.com>
2015-11-05 16:59:47 +0100

    init 2

---
+++ AngularForms/app/AngularFormsApp.js
@@ -0,0 +1,22 @@
+﻿
+var angularFormsApp = angular.module('angularFormsApp', ["ngRoute"]);
+
+angularFormsApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "app/Home.html",
+            controller: "HomeController"
+        })
+        .when("/newEmployeeForm", {
+            templateUrl: "app/EmployeeForm/efTemplate.html",
+            controller: "efController"
+        })
+        .otherwise({
+            redirectTo: "/home"
+        });
+});
+
+angularFormsApp.controller("HomeController",
+    function ($scope, $location) {
+
+    });
---
+++ AngularForms/app/EmployeeForm/efDirective.js
@@ -0,0 +1,10 @@
+﻿
+//employeeForm gets translated into employee-form in efTemplate.html etc
+angularFormsApp.directive('employeeForm',
+    function () {
+
+        return {
+            restrict: 'E',              // E means we only want to use this as an element
+            templateUrl: 'app/EmployeeForm/efTemplate.html'
+        }
+    });
